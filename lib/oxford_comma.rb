3f97efa82dd88eea4ff546dc62420f926fde37f3
Jonathan <jonathanmoorevla@gmail.com>
2023-02-04 11:41:36 -0600

    commit tree 7bfc2c26cd9cf9eced553699138bda713e63cc61

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length === 1
+        array[0]
+    elsif array.length === 2
+        array.join(" and ")
+    else 
+        oxford = array.join(", ")
+        last_space_index = oxford.rindex(", ")
+        oxford.insert(last_space_index + 1, " and")
+    end
 end
